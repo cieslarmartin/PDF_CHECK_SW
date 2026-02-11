caeb1f4c407ee6edbc842b1950ba294cc3c268ae
Cieslar Martin | CHVÁLEK ATELIÉR <cieslar@chvalekatelier.cz>
2026-02-11 23:08:18 +0100

    Update

--- web_app/version.py
+++ web_app/version.py
@@ -2,6 +2,6 @@
 # Jediné místo pro verzi webové aplikace. Formát: w{RR}.{MM}.{XXX}.
 # Při každé nasazené změně zvyšte XXX. Zobrazí se v patě webu a v Admin dashboardu.
 
-WEB_VERSION = "w26.02.003"
+WEB_VERSION = "w26.02.004"
 # Číselný build (zpětná kompatibilita)
-WEB_BUILD = 47
+WEB_BUILD = 48
